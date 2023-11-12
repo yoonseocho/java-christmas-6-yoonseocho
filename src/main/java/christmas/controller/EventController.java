ffa86118d32da4e4a25f17336e64b767d348327b
yoonseo <choyoonseo@joyunseoui-MacBookAir.local>
2023-11-13 01:14:32 +0900

    feat: 인사 출력

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,11 @@
+package christmas.controller;
+
+import christmas.view.OutputView;
+
+public class EventController {
+    public EventController() {
+    }
+    public void start(){
+        System.out.println(OutputView.START_MESSAGE);
+    }
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,5 @@
+package christmas.view;
+
+public class OutputView {
+    public static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+}
